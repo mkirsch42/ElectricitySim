a3b2bf5d1cbba0ae543058c38a67faaf2d9788f7
Mathew Kirschbaum <mkirsch42@gmail.com>
2017-02-03 14:54:32 -0500

    Add more presets

--- core/src/com/mkirsch42/electricity/ParticlePresets.java
+++ core/src/com/mkirsch42/electricity/ParticlePresets.java
@@ -7,7 +7,8 @@ import com.badlogic.gdx.graphics.Color;
 public final class ParticlePresets {
 
     public static final PresetFunction[] PRESETS = { ParticlePresets::test, ParticlePresets::figure8,
-	    ParticlePresets::binaryOrbit, ParticlePresets::h2, ParticlePresets::h2Bad };
+	    ParticlePresets::binaryOrbit, ParticlePresets::h2, ParticlePresets::h2Bad, ParticlePresets::inkjetPrinter,
+	    ParticlePresets::dipole, ParticlePresets::shell };
 
     public static void figure8(ArrayList<Particle> particles, float width, float height) {
 	particles.add(new Particle(width / 2, height / 2, 3000, Double.POSITIVE_INFINITY));
@@ -17,12 +18,13 @@ public final class ParticlePresets {
     }
 
     public static void h2(ArrayList<Particle> particles, float width, float height) {
-	particles.add(new Particle(width / 2, height / 2, 3000000, Double.POSITIVE_INFINITY));
-	particles.add(new Particle(width / 2 - 300, height / 2, 3000000, Double.POSITIVE_INFINITY));
-	particles.add(new Particle(width / 2, height / 2 + 100, -3, 3000));
+	particles.add(new Particle(width / 2, height / 2, 9000000, Double.POSITIVE_INFINITY));
+	particles.add(new Particle(width / 2 - 300, height / 2, 9000000, Double.POSITIVE_INFINITY));
+	particles.add(new Particle(width / 2, height / 2 + 100, -1, 3000));
 	particles.get(2).vx = 6.08855;
-	particles.add(new Particle(width / 2 - 300, height / 2 - 100, -3, 3000));
-	particles.get(3).vx = 6.08855;
+	particles.add(new Particle(width / 2 - 300, height / 2 - 100, -1, 3000));
+//	particles.get(3).vx = 6.08855;
+	particles.get(3).vx = 6.09515;
 	particles.get(3).c = Color.YELLOW;
 	particles.get(3).p = Color.YELLOW;
     }
@@ -42,6 +44,36 @@ public final class ParticlePresets {
 	particles.add(new Particle(width / 2 - 300, height / 2, -3000, 1000));
 	particles.get(0).vy = +2;
 	particles.get(1).vy = -2;
+    }
+
+    public static void inkjetPrinter(ArrayList<Particle> particles, float width, float height) {
+	for (int i = 0; i < 10; i++) {
+	    particles.add(new Particle(width / 2 - i * 50, height / 2, -300, Double.POSITIVE_INFINITY));
+	}
+	for (int i = 0; i < 10; i++) {
+	    particles.add(new Particle(width / 2 - i * 50, height / 2 - 150, 300, Double.POSITIVE_INFINITY));
+	}
+	particles.add(new Particle(width / 2 - 450, height / 2 - 25, -300, 1000));
+	particles.get(particles.size() - 1).vx = 15;
+	particles.get(particles.size() - 1).c = Color.YELLOW;
+    }
+
+    public static void dipole(ArrayList<Particle> particles, float width, float height) {
+	double q = 20001.51593409412635082844644784927368164065;
+	particles.add(new Particle(width / 2, height / 2, q, 1000));
+	particles.add(new Particle(width / 2 - 100, height / 2 - 100, -q, 1000));
+	ElectricitySim.gefX = 1;
+    }
+    
+    public static void shell(ArrayList<Particle> particles, float width, float height) {
+	double step = 1;
+	double r = 1000;
+	for(double i = 0; i < 360; i += step) {
+	    double x = Math.cos(Math.toRadians(i)) * r;
+	    double y = Math.sin(Math.toRadians(i)) * r;
+	    particles.add(new Particle(width / 2 + x, height / 2 + y, -10, Double.POSITIVE_INFINITY));
+	}
+	ElectricitySim.gefX = 1;
     }
 
     public static void test(ArrayList<Particle> particles, float width, float height) {
